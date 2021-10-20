8fb531575594fa9e409d45569fdb680401fdde9a
bilaldarwish8 <bilaldarwish88@gmail.com>
2021-10-20 17:02:35 +0300

    bilal

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 fetch('https://salty-shore-94674.herokuapp.com/users')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(json => {
+    var ul = document.getElementById('006')
+    json.forEach(element => {
+      var li = document.createElement('li')
+      li.innerHTML = element.name ;
+      ul.appendChild(li);
+
+    });
+  })
